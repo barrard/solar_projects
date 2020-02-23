80560d223c981ea838feae2b3388434c9d841939
barrard <barrard@gmail.com>
2020-02-22 14:26:23 -1000

    fixing res.isauth

--- server/middleware/router_middleware.js
+++ server/middleware/router_middleware.js
@@ -3,14 +3,14 @@ module.exports = {
 
    ensure_not_logged_in(req, res, next){
      logger.log('IS THIS USER ALREADY LOCGED IN??')
-     logger.log(!req.isAuthenticated ? true : false)
-    if(!req.isAuthenticated) return res.redirect('/account-overview')
+     logger.log(!req.isAuthenticated() ? true : false)
+    if(!req.isAuthenticated()) return res.redirect('/account-overview')
     next()
   },
 
 
    ensure_authenticated(req, res, next) {
-    if (req.isAuthenticated) {
+    if (req.isAuthenticated()) {
       return next();
     }
     logger.log('non-authenticated user being redirected'.bgWhite)
