1ddd179dce5dafe6386ee0eb4d8e7070550aa3dd
barrard <barrard@gmail.com>
2020-02-22 15:05:45 -1000

    Fixing something

--- server/middleware/router_middleware.js
+++ server/middleware/router_middleware.js
@@ -3,13 +3,15 @@ module.exports = {
 
    ensure_not_logged_in(req, res, next){
      logger.log('IS THIS USER ALREADY LOCGED IN??')
-     logger.log(!req.isAuthenticated() ? true : false)
-    if(!req.isAuthenticated()) return res.redirect('/account-overview')
+     logger.log(req.isAuthenticated() ? true : false)
+    if(req.isAuthenticated()) return res.redirect('/account-overview')
     next()
   },
 
 
    ensure_authenticated(req, res, next) {
+    logger.log(req.isAuthenticated())
+
     if (req.isAuthenticated()) {
       return next();
     }
